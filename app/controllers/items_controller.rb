bb98f5bc8ae6b3a78d07d9823fc301a8db949198
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-09-21 17:43:53 +0900

    エラーハンドリングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,16 +3,22 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :carriage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :price, format: { with: /\A[0-9]+\z/}
   end
+  validates :image, :name, :description, :category_id, :condition_id, :carriage_payer_id, :prefecture_id, :handling_time_id, presence: true 
+
   validates :price, inclusion: {in: 300..9999999}
+
+  validates :category_id, :condition_id, :carriage_payer_id, :prefecture_id, :handling_time_id, numericality: { other_than: 1 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :carriage_payer
