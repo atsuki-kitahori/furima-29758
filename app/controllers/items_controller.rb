e3b02a1343da714b9f82b3e6aa8aa7eb48698d27
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-09-22 15:22:49 +0900

    レビューの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :carriage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :carriage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
   end
 end
