6a70facb93568027450458b17be22e37e79e3397
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-09-25 12:16:12 +0900

    商品購入画面のルーティング、コントローラーの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,12 @@
 class PurchasesController < ApplicationController
+  before_action :set_item
+  def index
+    @items = Item.all
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
