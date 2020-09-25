c6703a3c4ca2e005f9c1ca997a190fedea2de43b
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-09-25 19:22:25 +0900

    購入ボタンに対してイベント発火

---
+++ app/javascript/card.js
@@ -0,0 +1,7 @@
+const pay =  ()=> {
+  Payjp.setPublickey("pk_test_ddd00e2b8369b1e18275d2ac")
+  const form = document.getElementById("cahrge-form");
+  form.addEventListener("submit",  () => {
+  })
+}
+window.addEventListener("load", pay);
