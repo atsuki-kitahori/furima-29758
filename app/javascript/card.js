4f4f7e74ab7599c9504987f99d7d58d20ce71109
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-09-28 12:01:06 +0900

    カード情報の入手

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,20 @@
 const pay =  ()=> {
-  Payjp.setPublickey("pk_test_ddd00e2b8369b1e18275d2ac")
-  const form = document.getElementById("cahrge-form");
-  form.addEventListener("submit",  () => {
+  Payjp.setPublicKey("pk_test_ddd00e2b8369b1e18275d2ac");
+  const form = document.getElementById("charge-form");
+  console.log(form)
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+    console.log(card);
   })
 }
-window.addEventListener("load", pay);
+window.addEventListener("load", pay); 
