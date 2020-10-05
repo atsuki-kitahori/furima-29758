1fa245d35d4926bc7269c8fd9328796c6616f85b
klum-kan <70319590+klum-kan@users.noreply.github.com>
2020-10-05 12:14:29 +0900

    テストコード

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+  validates :post_code, :prefecture_id, :city, :address, :phone_number, presence: true
+
+  validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :phone_number, format: {with: /\A\d{11}\z/ }
+
+  validates :prefecture_id, numericality: { other_than: 1 }
+
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @address = FactoryBot.build(:address)
+  end
+  describe '商品購入機能' do
+    
+    it '配送先の情報として、郵便番号・都道府県・市区町村・番地・電話番号が入力されていたら購入できること' do
+      expect(@address).to be_valid
+    end
+
+    it '配送先の情報として郵便番号・都道府県・市区町村・番地・電話番号のどれか一つでも入力されていなかったら購入できないこと' do
+      @address.post_code = nil
+      @address.prefecture_id = nil
+      @address.city = nil
+      @address.address = nil
+      @address.phone_number = nil
+      @address.valid?
+      expect(@address.errors.full_messages).to include("Post code can't be blank",
+        "Post code is invalid. Include hyphen(-)",
+        "Prefecture can't be blank",
+        "Prefecture is not a number",
+        "City can't be blank",
+        "Address can't be blank",
+        "Phone number can't be blank",
+        "Phone number is invalid")
+    end
+
+    it '郵便番号にはハイフンが必要であること' do
+      @address.post_code = "1234567"
+      @address.valid?
+      expect(@address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+    end
+
+    it '電話番号はハイフン不要で11桁以内である事' do
+      @address.phone_number = "090-1234-56789"
+      @address.valid?
+      expect(@address.errors.full_messages).to include("Phone number is invalid")
+    end
+
+    
+
+
+  end
 end
